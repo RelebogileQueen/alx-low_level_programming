ee44d15972cf0ce598dba1c63dfdf7cdfab58535
Relebogile <Mkhwanarelebogile@gmail.com>
2023-03-23 12:01:21 -0700

    more functions nested loops

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -10,7 +10,8 @@ void print_triangle(int size)
 if (size <= 0)
 {
 _putchar('\n');
-} else
+}
+else
 {
 int i, j;
 for (i = 1; i <= size; i++)
@@ -21,9 +22,9 @@ _putchar(' ');
 }
 for (j = 1; j <= i; j++)
 {
- _putchar('#')
+_putchar('#')
 }
- _putchar('\n');
+_putchar('\n');
 }
 }
 }
