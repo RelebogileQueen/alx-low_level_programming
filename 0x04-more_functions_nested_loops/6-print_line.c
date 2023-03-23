6363d880aa5e0f21c07a7d1815b44a9c3d5eed94
Relebogile <Mkhwanarelebogile@gmail.com>
2023-03-23 10:52:09 -0700

    more function nested loops

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,26 +1,23 @@
 #include "main.h"
 
 /**
-* print_square - prints a square, followed by a new line;
-* @size: size of the square
+* print_line - draws a straight line in the terminal
+* @n: number of times the character _ should be printed
 */
 
-void print_square(int size)
+void print_line(int n)
 {
-if (size <= 0)
+if (n <= 0)
 {
 _putchar('\n');
 } 
 else
-{ 
-int i, j;
-for (i = 0; i < size; i++)
 {
-for (j = 0; j < size; j++)
+int i;
+for (i = 1; i <= n; i++)
 {
-_putchar('#');
+_putchar('_');
 }
- _putchar('\n');
-}
+_putchar('\n');
 }
 }
