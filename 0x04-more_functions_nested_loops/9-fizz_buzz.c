a865a2543250acebf42837c8591941eb7fda1169
Relebogile <Mkhwanarelebogile@gmail.com>
2023-03-23 11:51:03 -0700

    more functions nested loops

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,3 +1,4 @@
+#include "main.h"
 #include <stdio.h>
 
 /**
@@ -15,24 +16,25 @@ for (i = 1; i <= 100; i++)
 if (i % 3 == 0 && i % 5 != 0)
 {
 printf(" Fizz");
-}
+} 
 else if (i % 5 == 0 && i % 3 != 0)
 {
 printf(" Buzz");
-}
+} 
 else if (i % 3 == 0 && i % 5 == 0)
 {
 printf(" FizzBuzz");
-{
+} 
 else if (i == 1)
 {
 printf("%d", i);
-}
+} 
 else
 {
 printf(" %d", i);
 }
 }
 printf("\n");
+
 return (0);
 }
