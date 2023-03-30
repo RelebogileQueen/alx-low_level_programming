52d565fcc5f03c375c0cbbee32590f1493ebd56c
Relebogile <Mkhwanarelebogile@gmail.com>
2023-03-30 15:32:09 -0700

    more pointers

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,15 +1,29 @@
 #include "main.h"
 
 /**
-* swap_int - swaps the values of two integers
-* @a: integer to swap
-* @b: integer to swap
+* _strncat - concatenate two strings
+* using at most n bytes from src
+* @dest: input value
+* @src: input value
+* @n: input value
+* Return: dest
 */
 
-void swap_int(int *a, int *b)
+char *_strncat(char *dest, char *src, int n)
 {
-int m;
-m = *a;
-*a = *b;
-*b = m;
+int i;
+int j;
+i = 0;
+while (dest[i] != '\0')
+{
+i++;
+j = 0;
+while (j < n && src[j] != '\0')
+{
+dest[i] = src[j];
+i++;
+j++;
+}
+dest[i] = '\0';
+return (dest);
 }
