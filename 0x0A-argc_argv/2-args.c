c317d9f0236ccd32db79d7a5cc754d30a54b9dd2
RelebogilQueen <mkhwanazirelebogile@gmail.com>
2023-06-12 17:22:33 -0700

    argc

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -6,14 +6,14 @@
 * @argc: number of arguments
 * @argv: array of arguments
 * Return: Always 0 (Success)
-*/                
+*/
 
 int main(int argc, char *argv[])
 {
-int i;        
+int i;
 for (i = 0; i < argc; i++)
 {
 printf("%s\n", argv[i]);
-}           
+}
 return (0);
 }
